bcf283ae7e36d14c377532268f3afa9c1e5972ae
masao.setogawa <ms.job.hunting@gmail.com>
2023-01-26 00:38:44 +0900

    罠スクリプト完成

--- app.js
+++ app.js
@@ -1,41 +1,13 @@
 const express = require("express");
 const session = require("express-session");
 const app = express();
-const port = 3001;
+const port = 3002;
 
 // htmlテンプレートエンジンを設定
 app.set("view engine", "ejs");
 
-// sessionの設定
-app.use(
-  session({
-    secret: "secret_key",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-// requestのbodyから値を取得する設定
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get("/", (req, res) => {
-  const userId = "yamada";
-  // セッションにユーザーIDを格納
-  req.session.userId = userId;
-  var data = {};
-  data.userId = userId;
-  res.render("./index.ejs", data);
-});
-
-app.get("/change", (req, res) => {
-  res.render("./change.ejs");
-});
-
-app.post("/submit", (req, res) => {
-  var data = {};
-  data.userId = req.session.userId;
-  data.password = req.body.pwd;
-  res.render("./result.ejs", data);
+app.get("/change-trap", (req, res) => {
+  res.render("./change-trap.ejs");
 });
 
 app.listen(port, () => {
